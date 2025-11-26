f74b02349f0dc66e530245603c20394c18cd43de
PedroSegalin12 <pedro.segalin@alunos.sc.senac.br>
2025-11-26 19:50:58 -0300

    4.0

--- app/Controllers/Admin/MontadoraController.php
+++ app/Controllers/Admin/MontadoraController.php
@@ -6,8 +6,8 @@ use App\Core\Csrf;
 use App\Core\Flash;
 use App\Core\View;
 use App\Repositories\MontadoraRepository;
-use App\Repositories\carroRepository;
-use App\Services\Montadoraservice;
+use App\Repositories\CarroRepository;
+use App\Services\MontadoraService;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -16,16 +16,16 @@ class MontadoraController
 {
     private View $view;
     private MontadoraRepository $repo;
-    private Montadoraservice $service;
+    private MontadoraService $service;
 
-    private carroRepository $productRepo;
+    private CarroRepository $productRepo;
 
     public function __construct()
     {
         $this->view = new View();
         $this->repo = new MontadoraRepository();
-        $this->service = new Montadoraservice();
-        $this->productRepo = new carroRepository();
+        $this->service = new MontadoraService();
+        $this->productRepo = new CarroRepository();
     }
 
     public function index(Request $request): Response
@@ -35,13 +35,13 @@ class MontadoraController
         $total = $this->repo->countAll();
         $Montadoras = $this->repo->paginate($page, $perPage);
         $pages = (int)ceil($total / $perPage);
-        $html = $this->view->render('admin/Montadoras/index', compact('Montadoras', 'page', 'pages'));
+        $html = $this->view->render('admin/montadoras/index', compact('montadoras', 'page', 'pages'));
         return new Response($html);
     }
 
     public function create(): Response
     {
-        $html = $this->view->render('admin/Montadoras/create', ['csrf' => Csrf::token(), 'errors' => []]);
+        $html = $this->view->render('admin/montadoras/create', ['csrf' => Csrf::token(), 'errors' => []]);
         return new Response($html);
     }
 
@@ -50,12 +50,12 @@ class MontadoraController
         if (!Csrf::validate($request->request->get('_csrf'))) return new Response('Token CSRF inválido', 419);
         $errors = $this->service->validate($request->request->all());
         if ($errors) {
-            $html = $this->view->render('admin/Montadoras/create', ['csrf' => Csrf::token(), 'errors' => $errors, 'old' => $request->request->all()]);
+            $html = $this->view->render('admin/montadoras/create', ['csrf' => Csrf::token(), 'errors' => $errors, 'old' => $request->request->all()]);
             return new Response($html, 422);
         }
         $Montadora = $this->service->make($request->request->all());
         $id = $this->repo->create($Montadora);
-        return new RedirectResponse('/admin/Montadoras/show?id=' . $id);
+        return new RedirectResponse('/admin/montadoras/show?id=' . $id);
     }
 
     public function show(Request $request): Response
@@ -63,7 +63,7 @@ class MontadoraController
         $id = (int)$request->query->get('id', 0);
         $Montadora = $this->repo->find($id);
         if (!$Montadora) return new Response('Montadora não encontrada', 404);
-        $html = $this->view->render('admin/Montadoras/show', ['Montadora' => $Montadora]);
+        $html = $this->view->render('admin/montadoras/show', ['Montadora' => $Montadora]);
         return new Response($html);
     }
 
@@ -72,7 +72,7 @@ class MontadoraController
         $id = (int)$request->query->get('id', 0);
         $Montadora = $this->repo->find($id);
         if (!$Montadora) return new Response('Montadora não encontrada', 404);
-        $html = $this->view->render('admin/Montadoras/edit', ['Montadora' => $Montadora, 'csrf' => Csrf::token(), 'errors' => []]);
+        $html = $this->view->render('admin/montadoras/edit', ['Montadora' => $Montadora, 'csrf' => Csrf::token(), 'errors' => []]);
         return new Response($html);
     }
 
@@ -82,13 +82,13 @@ class MontadoraController
         $data = $request->request->all();
         $errors = $this->service->validate($data);
         if ($errors) {
-            $html = $this->view->render('admin/Montadoras/edit', ['Montadora' => array_merge($this->repo->find((int)$data['id']), $data), 'csrf' => Csrf::token(), 'errors' => $errors]);
+            $html = $this->view->render('admin/montadoras/edit', ['Montadora' => array_merge($this->repo->find((int)$data['id']), $data), 'csrf' => Csrf::token(), 'errors' => $errors]);
             return new Response($html, 422);
         }
         $Montadora = $this->service->make($data);
         if (!$Montadora->id) return new Response('ID inválido', 422);
         $this->repo->update($Montadora);
-        return new RedirectResponse('/admin/Montadoras/show?id=' . $Montadora->id);
+        return new RedirectResponse('/admin/montadoras/show?id=' . $Montadora->id);
     }
 
     public function delete(Request $request): Response
@@ -103,6 +103,6 @@ class MontadoraController
         if (!Csrf::validate($request->request->get('_csrf'))) return new Response('Token CSRF inválido', 419);
         $id = (int)$request->request->get('id', 0);
         if ($id > 0) $this->repo->delete($id);
-        return new RedirectResponse('/admin/Montadoras');
+        return new RedirectResponse('/admin/montadoras');
     }
 }
